58711e4f961a02f29dd3d92fcb98ebb907a8a610
trippymajo <elyadesu@yandex.ru>
2025-04-23 15:50:41 +0300

    http files renamed

---
+++ TrTracker/TrtApiService/Models/Result.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TR_Tracker.Models
+{
+    public class Result
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] // Let it be string at first, then i need my own enum
+        public string Outcome { get; set; } = string.Empty;
+        
+        public string? ErrMsg { get; set; }
+
+        // Foreign Key
+        public int TestrunId { get; set; }
+        public Testrun Testrun { get; set; } = null!;
+
+        // Foreign Key
+        public int TestId { get; set; }
+        public Test Test { get; set; } = null!;
+    }
+}
