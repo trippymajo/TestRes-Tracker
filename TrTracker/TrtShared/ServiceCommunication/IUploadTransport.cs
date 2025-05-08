e4c10ae8569fe895d628a7ea9c297c9c9e76c008
trippymajo <elyadesu@yandex.ru>
2025-05-08 19:23:59 +0300

    Renamed IResultTransport to IUploadTransport

---
+++ TrTracker/TrtShared/ServiceCommunication/IUploadTransport.cs
@@ -0,0 +1,20 @@
+﻿using TrtShared.DTO;
+
+namespace TrtShared.ResultTransport
+{
+    public interface IUploadTransport
+    {
+        /// <summary>
+        /// Publishes path of the saved file to Parsing service
+        /// </summary>
+        /// <param name="path">Full file path of the saved file</param>
+        public Task PublishPathToFileAsync(string path);
+
+        /// <summary>
+        /// Subscribes to Parser service in order to get Parsed Dto
+        /// </summary>
+        /// <param name="timeout">Timeout value to count</param>
+        /// <returns>Dto of the parsed file</returns>
+        public Task<TestRunDTO?> GetParsedDtoAsync(TimeSpan timeout);
+    }
+}
