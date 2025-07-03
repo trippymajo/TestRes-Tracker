caefda36a39d4e2fe1b51740100d7abb9c062612
trippymajo <elyadesu@yandex.ru>
2025-07-03 12:33:59 +0300

    New migration after Navigation and cascade fix

---
+++ TrTracker/TrtApiService/Migrations/20250703093313_CascadeAndNavigation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrtApiService.Migrations
+{
+    /// <inheritdoc />
+    public partial class CascadeAndNavigation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Tests_TestId",
+                table: "Results");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Tests_TestId",
+                table: "Results",
+                column: "TestId",
+                principalTable: "Tests",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Tests_TestId",
+                table: "Results");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Tests_TestId",
+                table: "Results",
+                column: "TestId",
+                principalTable: "Tests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
